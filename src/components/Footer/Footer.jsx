07c4b13ea412b82065a0ffa24abc9c6637b62553
Jiho Sohn <jiho@HummingBird.attlocal.net>
2024-06-20 00:55:09 -0400

    UPDATED mozart's ghost

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,7 +13,7 @@ export default function Footer() {
       </Row>
       </Container>
 
-      <p id="mozarts-ghost"><a href="https://notiempo.lol/mozartsghost/13"
+      <p id="mozarts-ghost"><a href="https://jsohndata.github.io/no-tiempo/mozartsghost?id=13"
         alt="Click on it and press control-shift."
         title="Click on it and press control-shift.">π</a></p>
     </footer>
